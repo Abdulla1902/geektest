c1cb45f6d0e51f7dc9a7ab16172df1efa9b6c6b2
satta <sattarovabdulla@gmail.com>
2023-09-30 12:44:53 +0600

    commit

--- geektest/urls.py
+++ geektest/urls.py
@@ -3,6 +3,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include('geekapp.urls')),  # Замените 'todo_app' на имя вашего приложения
+    path('api/', include('geekapp.urls')),
 ]
 
